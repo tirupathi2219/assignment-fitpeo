151ae51995aee4399b64e17db34c89dddfc594dd
tirupathi2219 <tirupathi2219@gmail.com>
2023-11-03 10:54:42 +0530

    imp_sales_section

---
+++ src/components/DBSalesSection/SalesSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { salesSection } from '../../helpers/common';
+
+import styles from './SalesSection.module.scss'
+
+export default function SalesSection() {
+    return (
+        <div className={`${styles.sales_container}`}>
+            {salesSection.map((item, index) => {
+                return (
+                    <div className={`${styles.sales_subContainer}`}>
+                        <div className={`${styles.icon} ${styles[`icon${index}`]}`}>
+                            <i class={item.icon}></i>
+                        </div>
+                        <div>
+                            <p className={`${styles.stName}`}>{item.status_name}</p>
+                            <p className={`${styles.profit}`}>{item.profit}</p>
+                            <p>
+                                <span className={`${item.status ? `${styles.success}` : `${styles.failure}`}`}>
+                                    {
+                                        item.status
+                                            ? <i class="bi bi-arrow-up"></i>
+                                            : <i class="bi bi-arrow-down"></i>
+                                    }
+                                    {item.profit_percentage}
+                                </span>
+                                &nbsp;this month
+                            </p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
---
+++ src/helpers/common.js
@@ -0,0 +1,30 @@
+export const salesSection = [
+    {
+        icon: "bi bi-coin",
+        status_name: "Earning",
+        profit: "$198k",
+        profit_percentage: "37.8%",
+        status: true 
+    },
+    {
+        icon: "bi bi-card-list",
+        status_name: "Orders",
+        profit: "$2.4k",
+        profit_percentage: "2%",
+        status: false
+    },
+    {
+        icon: "bi bi-headset",
+        status_name: "Balance",
+        profit: "$2.4k",
+        profit_percentage: "2%",
+        status: false
+    },
+    {
+        icon: "bi bi-handbag",
+        status_name: "Total Sales",
+        profit: "$89k",
+        profit_percentage: "11%",
+        status: true
+    },
+]
