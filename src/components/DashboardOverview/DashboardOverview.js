76c039279976150c4223b65e5f54ac31d577d2a2
tirupathi2219 <tirupathi2219@gmail.com>
2023-12-21 18:10:36 -0800

    changes ready to push master

--- src/components/DashboardOverview/DashboardOverview.js
+++ src/components/DashboardOverview/DashboardOverview.js
@@ -3,21 +3,22 @@ import React from 'react';
 import SalesSection from '../DBSalesSection/SalesSection';
 
 import styles from './DashboardOverview.module.scss'
+
+export const handleChange = () => {
+    console.log('6== change');
+}
+
+export const handleBlur = () => {
+    console.log('9== blur');
+}
+
 export default function DashboardOverview() {
-
-    const handleChange = () => {
-        console.log('6== change');
-    }
-
-    const handleBlur = () => {
-        console.log('9== blur');
-    }
     const arr = [1, 2, 3, 4, 5]
     return (
         <>
             <header className={`d-flex justify-content-between`}>
                 <div>
-                    <h6>Welcome to my World <i className="bi bi-emoji-smile"></i>,</h6>
+                    <h6>Welcome to my World KRC <i className="bi bi-emoji-smile"></i>,</h6>
                 </div>
                 <div className={`${styles.search_container}`}>
                     <input type='text' placeholder='Search' onChange={handleChange} onBlur={handleBlur} />
